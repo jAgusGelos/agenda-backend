f2b309785bc55d02259a6f00a36fa669ec84c8ae
AgusGelos <juanagustingelos1@gmail.com>
2021-07-12 10:20:19 -0300

    project Scaffolding

---
+++ src/main.ts
@@ -0,0 +1,24 @@
+import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+declare const module: any;
+
+async function bootstrap() {
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('Agenda Manager API')
+    .setDescription('The Agenda Manager API Swagger')
+    .setVersion('1.0')
+    .addTag('Schedule')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+  await app.listen(3000);
+  if (module.hot) {
+    module.hot.accept();
+    module.hot.dispose(() => app.close());
+  }
+}
+bootstrap();
